985ad6491d7f0818c9a7b9149cc022e6c087fea1
AlphaZer00 <batudasdemir@gmail.com>
2024-01-08 12:47:15 -0500

    Add getPlayers, createPlayer, and checkSpaceBlank functions

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const loadBoard =(function() {
+const loadBoard = (function() {
     const createBoard = () => {
         const boardArray = [
             [0,0,0],
@@ -14,3 +14,25 @@ const loadBoard =(function() {
 
 loadBoard.createBoard();
 
+function getPlayers() {
+    const player1 = createPlayer("one");
+    const player2 = createPlayer("two");
+
+    let playerFirst = player1.name;
+    let playerSecond = player2.name;
+
+    return {playerFirst, playerSecond}
+} 
+
+function createPlayer(name) {
+    return {name};
+}
+
+function playRound(row, col) {
+    const checkSpaceBlank = () => {
+        const boardArray = loadBoard.createBoard();
+        if (boardArray[row][col] === 0) {
+            return true;
+        } else {return false;}
+    }
+}
