3321240e86f592813eb72953b82fe3143ae560af
AlphaZer00 <batudasdemir@gmail.com>
2024-01-10 11:47:06 -0500

    Add gameBoard IIFE

--- main.js
+++ main.js
@@ -1,38 +1,22 @@
-
-const loadBoard = (function() {
-    const createBoard = () => {
-        const boardArray = [
-            [0,0,0],
-            [0,0,0],
-            [0,0,0]
-        ]
-        return boardArray;
+const gameBoard = (function() {
+    const board = [];
+    for (let i = 0; i < 9; i++) {
+        board.push("");
     }
 
-    return {createBoard};
+    const setBoard = (i, sign) => {
+      board[i] = sign;
+    }
+      
+    const getBoard = (i) => {
+        return board[i];
+    }
+
+    const clearBoard = () => {
+        for (let i = 0; i < 9; i++) {
+            board[i] = "";
+        }
+    }
+
+    return {setBoard, getBoard, clearBoard};
 })();
-
-loadBoard.createBoard();
-
-function getPlayers() {
-    const player1 = createPlayer("one");
-    const player2 = createPlayer("two");
-
-    let playerFirst = player1.name;
-    let playerSecond = player2.name;
-
-    return {playerFirst, playerSecond}
-} 
-
-function createPlayer(name) {
-    return {name};
-}
-
-function playRound(row, col) {
-    const checkSpaceBlank = () => {
-        const boardArray = loadBoard.createBoard();
-        if (boardArray[row][col] === 0) {
-            return true;
-        } else {return false;}
-    }
-}
