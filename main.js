befeb05cfef8c997e8bf20682f3436d19993772d
AlphaZer00 <batudasdemir@gmail.com>
2024-01-10 12:16:17 -0500

    Add gameManager function with playRound & getCurrentPlayer subfunctions

--- main.js
+++ main.js
@@ -20,3 +20,31 @@ const gameBoard = (function() {
 
     return {setBoard, getBoard, clearBoard};
 })();
+
+const player = (sign) => {
+    this.sign = sign;
+    const getSign = () => {
+        return sign;
+    }
+    return {getSign};
+};
+
+const gameManager = (function() {
+    let isOver = false;
+    let round = 1;
+    const player1 = player('X');
+    const player2 = player('O');
+
+    const playRound = (index) => {
+        gameBoard.setBoard(index, getCurrentPlayer());
+    }
+    const getCurrentPlayer = () => {
+        if (round % 2) {
+            return player1.getSign();
+        } else {
+            return player2.getSign();
+        }
+    }
+
+    return {playRound, getCurrentPlayer}
+})();
